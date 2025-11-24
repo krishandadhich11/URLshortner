939ab8a90c414d42dab5bd0a0c17c37f0e0d7fb2
Krishan <krishandadhich11@gmail.com>
2025-11-24 14:10:16 +0530

    create model and schema

---
+++ models/model.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const urlSchema = new mongoose.Schema(
+  {
+    shortId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    redirectURL: {
+      type: String,
+      required: true,
+    },
+    totalClicks: {
+      visitHistory: [{ timeStamp: { type: Number } }],
+    }
+  },
+  { timestamps: true }
+);
+
+const URL = mongoose.model("url", urlSchema);
+export default URL;
